4a08d0f0ee1c0c187453040509d7c7941d53f350
Zach Tibbitts <ztibbitts@hchb.com>
2018-05-14 08:57:00 -0400

    handling

--- babyyet/views.py
+++ babyyet/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.contrib.auth.models import User
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
@@ -7,24 +9,26 @@ from .models import BabyYet
 
 
 def autoconfigure(request):
-    users = User.objects.all()
-    if users.count() != 0:
+    try:
+        users = User.objects.all()
+        if users.count() != 0:
+            raise Http404()
+
+        username = os.environ.get('ADMIN_USERNAME')
+        email = os.environ.get('ADMIN_EMAIL')
+        password = os.environ.get('ADMIN_PASSWORD')
+        if not username or not password:
+            raise Http404()
+
+        admin = User(username=username, email=email)
+        admin.set_password(password)
+        admin.is_superuser = True
+        admin.is_staff = True
+        admin.save()
+
+        return HttpResponseRedirect('/')
+    except:
         raise Http404()
-
-    username = os.environ.get('ADMIN_USERNAME')
-    email = os.environ.get('ADMIN_EMAIL')
-    password = os.environ.get('ADMIN_PASSWORD')
-    if not username or not password:
-        raise Http404()
-
-    admin = User(username=username, email=email)
-    admin.set_password(password)
-    admin.is_superuser = True
-    admin.is_staff = True
-    admin.save()
-
-    return HttpResponseRedirect('/')
-
 
 def home(request):
     try:
