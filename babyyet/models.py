3cfbe86d1223055fba35ec4c1efaeac1b937cc48
Zach Tibbitts <ztibbitts@hchb.com>
2018-06-27 10:57:54 -0400

    add field

--- babyyet/models.py
+++ babyyet/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class BabyYet(models.Model):
     baby_yet = models.BooleanField(default=False)
+    diapers_url = models.URLField(default=None, null=True, blank=True)
